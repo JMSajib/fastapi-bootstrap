f20256199f704742f588890a41ed2b78596dff90
jahidul <jahidul@nexgencloud.com>
2024-08-18 22:57:36 +0600

    Added description in swagger

--- src/admin/routes.py
+++ src/admin/routes.py
@@ -5,6 +5,7 @@ from starlette import status
 from src.db.databases import get_db
 from src.admin.service import AdminService
 from src.auth.dependencies import is_admin
+from pydantic import BaseModel
 
 admin_router = APIRouter()
 
@@ -17,8 +18,32 @@ def get_admin_service(db: session = Depends(get_db)) -> AdminService:
     return AdminService(db)
 
 
-# Routes
-@admin_router.get("/todo", status_code=status.HTTP_200_OK)
+# swagger response
+class SuccessResponseModel(BaseModel):
+    status: bool
+    message: str
+
+
+class ErrorResponseModel(BaseModel):
+    status: bool
+    message: str
+
+
+@admin_router.get(
+    "/todo",
+    status_code=status.HTTP_200_OK,
+    summary="Get All Todos",
+    description="Get all the todos that includes title, description, priority, complete and owner id",
+    responses={
+        200: {"description": "Success", "model": SuccessResponseModel},
+        400: {"description": "Bad Request", "model": ErrorResponseModel},
+        401: {"description": "Unauthorized", "model": ErrorResponseModel},
+        403: {"description": "Forbidden", "model": ErrorResponseModel},
+        404: {"description": "Not Found", "model": ErrorResponseModel},
+        500: {"description": "Internal Server Error"},
+    },
+    tags=["Admin"],
+)
 async def read_all(
     user: admin_dependency,
     admin_service: Annotated[AdminService, Depends(get_admin_service)],
@@ -26,7 +51,21 @@ async def read_all(
     return admin_service.get_all_todos()
 
 
-@admin_router.delete("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+@admin_router.delete(
+    "/todo/{todo_id}",
+    status_code=status.HTTP_200_OK,
+    summary="Delete A Todo",
+    description="Delete a todo by providing a todo id in the path.",
+    responses={
+        200: {"description": "Success", "model": SuccessResponseModel},
+        400: {"description": "Bad Request", "model": ErrorResponseModel},
+        401: {"description": "Unauthorized", "model": ErrorResponseModel},
+        403: {"description": "Forbidden", "model": ErrorResponseModel},
+        404: {"description": "Not Found", "model": ErrorResponseModel},
+        500: {"description": "Internal Server Error"},
+    },
+    tags=["Admin"],
+)
 async def delete_todo(
     user: admin_dependency,
     admin_service: Annotated[AdminService, Depends(get_admin_service)],
